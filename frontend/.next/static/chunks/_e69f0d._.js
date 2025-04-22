148807cd610e6b74733d5dc15ab6c2ec88095a34
Roy Canani <roy33441c@gmail.com>
2025-04-22 20:21:10 +0300

    init frontend

--- frontend/.next/static/chunks/_e69f0d._.js
+++ frontend/.next/static/chunks/_e69f0d._.js
@@ -5,12 +5,12 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/_e69f0d._.js",
   "chunks": [
-    "static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js",
-    "static/chunks/node_modules_next_dist_compiled_107ce8._.js",
-    "static/chunks/node_modules_next_dist_client_523921._.js",
-    "static/chunks/node_modules_next_dist_4bb10e._.js",
-    "static/chunks/node_modules_@swc_helpers_cjs_00636a._.js",
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_d0a96d._.js"
+    "static/chunks/a7abb_next_dist_compiled_react-dom_99acc9._.js",
+    "static/chunks/a7abb_next_dist_compiled_c9fc5a._.js",
+    "static/chunks/a7abb_next_dist_client_91ac0f._.js",
+    "static/chunks/a7abb_next_dist_98d156._.js",
+    "static/chunks/61dca_@swc_helpers_cjs_e27e0a._.js",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_d0afae._.js"
   ],
   "source": "entry"
 });
